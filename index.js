bd15a7df6c018992e230e62d44681af514072095
hermbit <hermitsmail@gmail.com>
2018-06-01 06:50:33 -0400

    init commit

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: 'standard',
+
+  rules: {
+    'space-before-function-paren': 'off',
+    'no-unused-vars': 'warn',
+    'eol-last': 'off',
+    'no-multiple-empty-lines': 'warn',
+    'no-trailing-spaces': 'warn'
+  }
+}
